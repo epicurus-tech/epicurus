a82c0b11277c12cfab7d563b381bc2aefbfc1f5c
Jessica de Lima Fileto <jessica.lima@gupy.com.br>
2020-05-30 19:39:26 -0300

    chore: init stryker configuration

---
+++ stryker.conf.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mutator: "javascript",
+  packageManager: "npm",
+  reporters: ["html", "clear-text", "progress", "dashboard"],
+  testRunner: "mocha",
+  transpilers: [],
+  testFramework: "mocha",
+  coverageAnalysis: "perTest",
+};
